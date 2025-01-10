90c413cc11d09e7b86260f07fc903212207e7106
sethchens <bryan31032@gmail.com>
2025-01-09 19:02:53 -0700

    Files added

---
+++ Hello World/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Seth on 1/9/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
